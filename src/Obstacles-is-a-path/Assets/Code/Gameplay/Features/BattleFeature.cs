ac04fb6edbba5bde0fe23867bc426ecf26048d66
Ivan Helsing <60290995+Ivan-Helsing@users.noreply.github.com>
2024-11-02 19:18:43 -0400

    Battle feature and ECS runner created.

---
+++ src/Obstacles-is-a-path/Assets/Code/Gameplay/Features/BattleFeature.cs
@@ -0,0 +1,13 @@
+﻿using Code.Gameplay.Features.Input;
+using Code.Infrastructure.Factory;
+
+namespace Code.Gameplay.Features
+{
+  public class BattleFeature : Feature
+  {
+    public BattleFeature(ISystemFactory systems)
+    {
+      Add(systems.Create<InputFeature>());
+    }
+  }
+}
---
+++ src/Obstacles-is-a-path/Assets/Code/Infrastructure/EcsRunner.cs
@@ -0,0 +1,34 @@
+﻿using Code.Gameplay.Features;
+using Code.Infrastructure.Factory;
+using UnityEngine;
+using Zenject;
+
+namespace Code.Infrastructure
+{
+  public class EcsRunner : MonoBehaviour
+  {
+    private BattleFeature _battleFeature;
+    private ISystemFactory _systems;
+
+    [Inject]
+    public void Construct(ISystemFactory systems) => 
+      _systems = systems;
+
+    private void Start()
+    {
+      _battleFeature = _systems.Create<BattleFeature>();
+      _battleFeature.Initialize();
+    }
+
+    private void Update()
+    {
+      _battleFeature.Execute();
+      _battleFeature.Cleanup();
+    }
+
+    private void OnDestroy()
+    {
+      _battleFeature.TearDown();
+    }
+  }
+}
