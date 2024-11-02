173ce6c94846760a0984ebd354355df13bdb265f
Ivan Helsing <60290995+Ivan-Helsing@users.noreply.github.com>
2024-11-02 12:06:03 -0400

    Code generated with Jenny

--- src/Obstacles-is-a-path/Assets/Code/Infrastructure/Bootstrap/StartFromInitialSceneInEditor.cs
+++ src/Obstacles-is-a-path/Assets/Code/Infrastructure/Bootstrap/StartFromInitialSceneInEditor.cs
@@ -1,21 +1,21 @@
-using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Zenject;
 
 namespace Code.Infrastructure.Bootstrap
 {
     public class StartFromInitialSceneInEditor : MonoBehaviour
     {
 #if UNITY_EDITOR
-      // private const int EnterSceneIndex = 1;
-      //
-      // private void Awake()
-      // {
-      //   if(ProjectContext.HasInstance)
-      //     return;
-      //
-      //   SceneManager.LoadScene(EnterSceneIndex);
-      // }
+      private const int EnterSceneIndex = 1;
+      
+      private void Awake()
+      {
+        if(ProjectContext.HasInstance)
+          return;
+      
+        SceneManager.LoadScene(EnterSceneIndex);
+      }
 #endif
     }
 }
