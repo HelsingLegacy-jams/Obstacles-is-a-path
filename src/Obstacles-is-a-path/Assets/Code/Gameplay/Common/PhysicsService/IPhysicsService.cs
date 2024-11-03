43651c1be100d26ce8f9c50b903afdf7b89bf2c3
Ivan Helsing <60290995+Ivan-Helsing@users.noreply.github.com>
2024-11-02 22:45:43 -0400

    Physics SphereCast created.

--- src/Obstacles-is-a-path/Assets/Code/Gameplay/Common/PhysicsService/IPhysicsService.cs
+++ src/Obstacles-is-a-path/Assets/Code/Gameplay/Common/PhysicsService/IPhysicsService.cs
@@ -1,9 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Code.Gameplay.Common.PhysicsService
 {
   public interface IPhysicsService
   {
     Vector3 Raycast(Vector2 point, Ray ray, int layer);
+    IEnumerable<GameEntity> SphereCast(Vector3 position, float radius, int layerMask);
   }
 }
--- src/Obstacles-is-a-path/Assets/Code/Gameplay/Common/PhysicsService/PhysicsService.cs
+++ src/Obstacles-is-a-path/Assets/Code/Gameplay/Common/PhysicsService/PhysicsService.cs
@@ -1,14 +1,43 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using Code.Gameplay.Common.Collisions;
+using UnityEngine;
 
 namespace Code.Gameplay.Common.PhysicsService
 {
   public class PhysicsService : IPhysicsService
   {
+    private static readonly Collider[] OverlapHits = new Collider[32];
+    
+    private readonly ICollisionRegistryEntity _collisionRegistry;
+
+    public PhysicsService(ICollisionRegistryEntity collisionRegistry)
+    {
+      _collisionRegistry = collisionRegistry;
+    }
+
     public Vector3 Raycast(Vector2 point, Ray ray, int layer)
     {
       return Physics.Raycast(ray, out RaycastHit hit, 20, layer) 
         ? hit.point 
         : Vector3.zero;
     }
+
+    public IEnumerable<GameEntity> SphereCast(Vector3 position, float radius, int layerMask)
+    {
+      int hitCount = OverlapSphere(position, radius, OverlapHits, layerMask);
+
+      for (int i = 0; i < hitCount; i++)
+      {
+        GameEntity entity = _collisionRegistry.Get<GameEntity>(OverlapHits[i].GetInstanceID());
+        
+        if (entity == null)
+          continue;
+        
+        yield return entity;
+      }
+    }
+
+    private int OverlapSphere(Vector3 worldPosition, float radius, Collider[] hits, int layerMask) => 
+      UnityEngine.Physics.OverlapSphereNonAlloc(worldPosition, radius, hits, layerMask);
   }
 }
