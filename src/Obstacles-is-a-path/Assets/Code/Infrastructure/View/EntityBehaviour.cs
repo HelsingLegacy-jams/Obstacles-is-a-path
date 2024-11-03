01ae6f577d6be51d7f594784f1cfffd3e60af380
Ivan Helsing <60290995+Ivan-Helsing@users.noreply.github.com>
2024-11-02 23:05:38 -0400

    EntityView created

---
+++ src/Obstacles-is-a-path/Assets/Code/Infrastructure/View/EntityBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using Code.Gameplay.Common.Collisions;
+using UnityEngine;
+using Zenject;
+
+namespace Code.Infrastructure.View
+{
+  public class EntityBehaviour : MonoBehaviour, IEntityView
+  {
+    private GameEntity _entity;
+    private ICollisionRegistry _collisionRegistry;
+
+    public GameEntity Entity => _entity;
+
+    [Inject]
+    public void Construct(ICollisionRegistry collisionRegistry) => 
+      _collisionRegistry = collisionRegistry;
+
+    public void SetEntity(GameEntity entity)
+    {
+      _entity = entity;
+      _entity.AddView(this);
+      _entity.Retain(this);
+
+      foreach (Collider collider in GetComponentsInChildren<Collider>(includeInactive: true)) 
+        _collisionRegistry.Register(collider.GetInstanceID(), _entity);
+    }
+
+    public void ReleaseEntity()
+    {
+      foreach (Collider collider in GetComponentsInChildren<Collider>(includeInactive: true)) 
+        _collisionRegistry.Unregister(collider.GetInstanceID());
+
+      _entity.Release(this);
+      _entity = null;
+    }
+  }
+}
